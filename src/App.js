4467e9ab06e2c239831cc538e533cb6017194fe0
adisk01 <adiscrazyO1@gmail.com>
2022-06-04 20:23:12 +0530

    inputDone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-let App = () => {
-  return (
-    <div>
-      <h1>This is our first react app</h1>
-      <p> some other html</p>
-    </div>
-  );
-}
+import React from "react";
+class App extends React.Component {
 
+  state = {
+    tasks : ["study dsa" , "study CP" , "Do codeforces" , "Do leetcode"],
+    currinput : "",
+  };
+  render = () => {
+    return(
+      <div>
+        <input type="text" onChange={(e) => {
+          this.setState({currinput : e.currentTarget.value });
+        }}
+        value={this.state.currinput}
+        onKeyDown = {(e)=> {
+          if(e.key=="Enter")
+          {
+            this.setState({
+              tasks : [...this.state.tasks , this.state.currinput],
+              currinput : "",
+            });
+          }
+        }}
+        />
+        
+        <ul>
+          {
+            this.state.tasks.map((el) => {
+              return <li>{el}</li>;
+            })
+          }
+        </ul>
+      </div>
+    );
+  };
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Mycomp from './Mycomp';
-import ClassComp from './ClassComp'
+
 ReactDOM.render(
             <div>
-              <ClassComp />
-              <ClassComp />
-              <ClassComp />
-              <ClassComp />
-              <ClassComp />
+              <App />
+              
             </div>,
             document.getElementById('root'));
 
